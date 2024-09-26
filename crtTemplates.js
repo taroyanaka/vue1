61859a7087f268fbd157ab868ad6b00565200224
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-09-26 17:13:25 +0900

    anyway

---
+++ crtTemplates.js
@@ -0,0 +1,26 @@
+const crtTemplates = [
+    { id: 1, name: 'ネズミー', emoji: '🐭', fu: 50, ha: 50, alive: true, pp: 0, type: 0, mPp: 30, gld: 5, price: 100, ev: [
+        { fu: 17.5, ha: 17.5, pp: 4, nNm: 'ネズミ王', nEm: '🐭👑', nGd: 10, mPp: 28 },
+        { fu: 35, ha: 35, pp: 8, nNm: 'ネズミ大王', nEm: '🐭👑👑', nGd: 20, mPp: 26 },
+        { fu: 52.5, ha: 52.5, pp: 12, nNm: 'ネズミ皇帝', nEm: '🐭👑👑👑', nGd: 30, mPp: 24 },
+        { fu: 70, ha: 70, pp: 16, nNm: 'ネズミ神', nEm: '🐭😇', nGd: 40, mPp: 22 }
+    ], rem: 10, pb: 100, message: '' },
+    { id: 2, name: 'ネコー', emoji: '🐱', fu: 50, ha: 50, alive: true, pp: 0, type: 0, mPp: 45, gld: 7, price: 1000, ev: [
+        { fu: 20, ha: 20, pp: 6, nNm: 'ネコ王', nEm: '🐱👑', nGd: 14, mPp: 42 },
+        { fu: 40, ha: 40, pp: 12, nNm: 'ネコ大王', nEm: '🐱👑👑', nGd: 28, mPp: 39 },
+        { fu: 60, ha: 60, pp: 18, nNm: 'ネコ皇帝', nEm: '🐱👑👑👑', nGd: 42, mPp: 36 },
+        { fu: 80, ha: 80, pp: 24, nNm: 'ネコ神', nEm: '🐱😇', nGd: 56, mPp: 33 }
+    ], rem: 10, pb: 100, message: '' },
+    { id: 3, name: 'イヌー', emoji: '🐶', fu: 50, ha: 50, alive: true, pp: 0, type: 0, mPp: 60, gld: 9, price: 10000, ev: [
+        { fu: 22.5, ha: 22.5, pp: 8, nNm: 'イヌ王', nEm: '🐶👑', nGd: 18, mPp: 56 },
+        { fu: 45, ha: 45, pp: 16, nNm: 'イヌ大王', nEm: '🐶👑👑', nGd: 36, mPp: 52 },
+        { fu: 67.5, ha: 67.5, pp: 24, nNm: 'イヌ皇帝', nEm: '🐶👑👑👑', nGd: 54, mPp: 48 },
+        { fu: 85, ha: 85, pp: 32, nNm: 'イヌ神', nEm: '🐶😇', nGd: 72, mPp: 44 }
+    ], rem: 10, pb: 100, message: '' },
+    { id: 4, name: '未知のクリーチャー', emoji: '🦄', fu: 50, ha: 50, alive: true, pp: 0, type: 0, mPp: 90, gld: 12, price: 100000, ev: [
+        { fu: 25, ha: 25, pp: 12, nNm: '王', nEm: '🐒', nGd: 24, mPp: 85 },
+        { fu: 50, ha: 50, pp: 24, nNm: '大王', nEm: '🚶', nGd: 48, mPp: 80 },
+        { fu: 75, ha: 75, pp: 36, nNm: '皇帝', nEm: '🕺', nGd: 72, mPp: 75 },
+        { fu: 90, ha: 90, pp: 48, nNm: '神', nEm: '😇', nGd: 96, mPp: 70 }
+    ], rem: 10, pb: 100, message: '' },
+];
