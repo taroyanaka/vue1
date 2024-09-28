b294ea3d82dc29da0f9796bd8b2c1870ed501f96
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2024-09-28 18:28:10 +0900

    anyway

--- WethePeopleoftheUnitedStates.js
+++ WethePeopleoftheUnitedStates.js
@@ -1,5 +1,5 @@
 // https://constitutionus.com/
-const WethePeopleoftheUnitedStates = 
+const english_text = 
 `We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.
 
 Article I (Article 1 - Legislative)
